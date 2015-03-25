e491e4294b048d4ddad84c9964e41d2459253c35
leonardofontourarubio@gmail.com <leonardofontourarubio@gmail.com>
2015-03-25 15:38:47 -0300

    leo 002

---
+++ Leo/Xcode6Tutorial/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  Xcode6Tutorial
+//
+//  Created by Leonardo Rubio 1 on 3/24/15.
+//  Copyright (c) 2015 FantasyName. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstCardImageView: UIImageView!
+    @IBOutlet weak var secondCardImageView: UIImageView!
+    @IBOutlet weak var playRoundButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    
+    @IBOutlet weak var PlayerScore: UILabel!
+    @IBOutlet weak var EnemyScore: UILabel!
+    
+    
+    var cardNamesArray:[String] = ["Card1Cassio", "Card2FabioSantos", "Card3Fagner", "Card4Gil", "Card5Felipe", "Card6Elias", "Card7Ralf", "Card8Jadson", "Card9RenatoAugusto", "Card10VagnerLove", "Card11Malcom", "Card12Emerson", "Card13Danilo", "Card14Guerrero"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //self.playRoundButton.setTitle("Play", forState: UIControlState.Normal)
+    }
+
+    var playerScoreTotal = 0;
+    var enemyScoreTotal = 0;
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func playRoundTapped(sender: UIButton) {
+        
+        
+        // randomize um numero para o firstImageView
+        var firstRandomNumber:Int = Int(arc4random_uniform(13))
+        
+        //Controi uma string com numero randomico
+        //var firstCardString:String = String(format: "Card%i", firstRandomNumber)
+        
+        //Acessa as cartas por array
+        var firstCardString:String = self.cardNamesArray[firstRandomNumber]
+        
+        // Seta a firstCardView com o numero correspondente
+        self.firstCardImageView.image = UIImage (named: firstCardString)
+        
+        
+        
+        
+        //randomize a number for secondCardView
+        var secondRandomNumber:Int = Int (arc4random_uniform(13))
+        
+        //constroi string com random number pra secondCardView
+        //var secondCardString:String = String(format: "Card%i", secondRandomNumber)
+        
+        //acessa cartas por array
+        var secondCardString:String = self.cardNamesArray [secondRandomNumber]
+        
+        // Seta secondCardView com numero correspondente
+        self.secondCardImageView.image = UIImage (named: secondCardString)
+        
+        //determina a carta maior
+        if firstRandomNumber > secondRandomNumber {
+            playerScoreTotal += 1
+            self.PlayerScore.text = String(playerScoreTotal)
+            
+        }
+        else if firstRandomNumber == secondRandomNumber {
+        
+        }
+        else {
+            enemyScoreTotal += 1
+            self.EnemyScore.text = String (enemyScoreTotal)
+        }
+    }
+
+}
+
+
+
+
+
+
+
